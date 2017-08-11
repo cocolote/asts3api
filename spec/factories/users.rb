fac36da697f95bfc388bdb3db928f39d78458cb8
Ezequiel Lopez <ezelop@astutesolutions.com>
2017-08-11 13:19:33 -0400

    users authentication done

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     username { Faker::Internet.user_name }
-    password_digest { Faker::Internet.password }
+    password { Faker::Internet.password }
     email { Faker::Internet.free_email }
     first_name { Faker::StarTrek.character }
     last_name { Faker::StarWars.character }
